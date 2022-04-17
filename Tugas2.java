24d790dcb0d9778897cdbf742d27c13e517c1b79
20104221 <101607717+20104221@users.noreply.github.com>
2022-04-18 00:09:01 +0800

    setoran Tugas2_oop

---
+++ Tugas2.java
@@ -0,0 +1,14 @@
+
+package T2;
+
+/**
+ *
+ * @sultanaderamadhani 20104221
+ */
+public class Tugas2 {
+    public static void main(String[] args) {
+        volumebalok balok1 = new volumebalok(8,6,5);
+        balok1.cetakvolume ();
+    }
+    
+}
